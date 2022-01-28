9ec7edda5f4a17f713c1e19b2fa98f42eb41ec54
NeilLiu030107 <liuneil030107@gmail.com>
2022-01-28 15:47:42 -0800

    Update HoodZeroing
    1.Separate hoodzeroing into two commands, which will executed by a sequential command group.
    2. Add a timer to HoodZeroingHardStop.
    3. HoodZeroingHardStop will set encoder value to the physical 0 angle.

--- src/main/java/team3647/frc2022/commands/HoodZeroingHardstop.java
+++ src/main/java/team3647/frc2022/commands/HoodZeroingHardstop.java
@@ -7,64 +7,71 @@ package team3647.frc2022.commands;
 import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import team3647.frc2022.subsystems.Hood;
+import team3647.lib.wpi.Timer;
 
-public class HoodZeroing extends CommandBase {
+;
+
+public class HoodZeroingHardstop extends CommandBase {
     private final Hood hood;
+    private final Timer timer;
     private MedianFilter filter;
-    private double medianVelocity;
-    private boolean reachHighVelocity = false;
-    private boolean haveReachedHighVelocity = false;
-    private boolean reachHardStop = false;
     private final double lowVelocityThreshold;
-    private final double highVelocityThreshold;
     private final double output;
-
-    /** Creates a new HoodZeroing. */
-    public HoodZeroing(
+    private final double time;
+    private double medianVelocity;
+    private boolean reachLowVelocity = false;
+    private boolean reachHardStop = false;
+    /** Creates a new HoodZeroingHardstop. */
+    public HoodZeroingHardstop(
             Hood hood,
             double output,
-            int sampleSize,
+            MedianFilter filter,
             double lowVelocityThreshold,
-            double highVelocityThreshold) {
+            double time) {
         this.hood = hood;
         this.output = output;
-        this.highVelocityThreshold = highVelocityThreshold;
+        this.filter = filter;
         this.lowVelocityThreshold = lowVelocityThreshold;
-        filter = new MedianFilter(sampleSize);
+        this.time = time;
+        timer = new Timer();
         addRequirements(hood);
         // Use addRequirements() here to declare subsystem dependencies.
     }
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {
+        timer.reset();
+    }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
         this.hood.setOpenloop(output);
         medianVelocity = filter.calculate(hood.getVelocity());
-        reachHighVelocity = (medianVelocity >= highVelocityThreshold);
-        if (reachHighVelocity) {
-            haveReachedHighVelocity = true;
-        }
-        reachHardStop =
+        reachLowVelocity =
                 (medianVelocity <= lowVelocityThreshold && medianVelocity >= -lowVelocityThreshold);
+        if (reachHardStop) {
+            timer.start(); // no-op when timer is already running
+            if (timer.get() > time) {
+                reachHardStop = true;
+            }
+
+        } else {
+            timer.reset();
+        }
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        this.hood.end();
-        this.hood.resetEncoder();
+        this.hood.setOpenloop(0);
+        this.hood.setEncoder(0); // set encoder to physically 0 angle
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        if (reachHardStop && haveReachedHighVelocity) {
-            return true;
-        }
-        return false;
+        return reachHardStop;
     }
 }
---
+++ src/main/java/team3647/frc2022/commands/HoodZeroingHighVel.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team3647.frc2022.commands;
+
+import edu.wpi.first.math.filter.MedianFilter;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team3647.frc2022.subsystems.Hood;
+
+public class HoodZeroingHighVel extends CommandBase {
+    private final Hood hood;
+    private MedianFilter filter;
+    private double medianVelocity;
+    private boolean reachHighVelocity = false;
+    private final double highVelocityThreshold;
+    private final double output;
+
+    /** Creates a new HoodZeroing. */
+    public HoodZeroingHighVel(
+            Hood hood, double output, MedianFilter filter, double highVelocityThreshold) {
+        this.hood = hood;
+        this.output = output;
+        this.filter = filter;
+        this.highVelocityThreshold = highVelocityThreshold;
+        addRequirements(hood);
+        // Use addRequirements() here to declare subsystem dependencies.
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        this.hood.setOpenloop(output);
+        medianVelocity = filter.calculate(hood.getVelocity());
+        reachHighVelocity = (medianVelocity >= highVelocityThreshold);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return reachHighVelocity;
+    }
+}
