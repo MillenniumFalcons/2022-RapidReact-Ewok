e434f3368e1284def7bcdb0cfe19d76bd247065d
NeilLiu030107 <liuneil030107@gmail.com>
2022-01-29 20:51:39 -0800

    Final fix with HoodZeroing
    Reset filter when initialize HoodZeroingHighVel command

--- src/main/java/team3647/frc2022/commands/HoodZeroingHighVel.java
+++ src/main/java/team3647/frc2022/commands/HoodZeroingHighVel.java
@@ -29,7 +29,9 @@ public class HoodZeroingHighVel extends CommandBase {
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {
+        filter.reset();
+    }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
