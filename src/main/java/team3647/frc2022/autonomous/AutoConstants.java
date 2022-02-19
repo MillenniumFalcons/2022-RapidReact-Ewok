4462a5e23beebbd0009b98120f9be5427aacd644
amoizhao <zhaoa6125@gmail.com>
2022-02-19 13:15:06 -0800

    add auto constants and trajectories

--- src/main/java/team3647/frc2022/autonomous/AutoConstants.java
+++ src/main/java/team3647/frc2022/autonomous/AutoConstants.java
@@ -1,3 +1,46 @@
 package team3647.frc2022.autonomous;
 
-public class AutoConstants {}
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+
+// previous contants.cField
+public class AutoConstants {
+    // acceleration values
+    public static final double kMaxSpeedMetersPerSecond = 2;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 2;
+    public static final double slowBoiAutoAccel = 1;
+    public static final double regBoiAutoAccel = 3;
+    public static final double fastBoiAutoAccel = 4;
+
+    // 2022 5 ball
+    public static final Pose2d positionOnTarmacParallel =
+            new Pose2d(
+                    Units.inchesToMeters(296.541),
+                    Units.inchesToMeters(66.578),
+                    new Rotation2d(Units.degreesToRadians(90)));
+    public static final Pose2d bottomLeftBall1 =
+            new Pose2d(
+                    Units.inchesToMeters(296.541),
+                    Units.inchesToMeters(18.21),
+                    new Rotation2d(Units.degreesToRadians(90)));
+    public static final Translation2d bottomLeftBall2 =
+            new Translation2d(Units.inchesToMeters(197.593), Units.inchesToMeters(73.11));
+    public static final Pose2d bottomLeftBall3 =
+            new Pose2d(
+                    Units.inchesToMeters(40),
+                    Units.inchesToMeters(45.684),
+                    new Rotation2d(Units.degreesToRadians(45)));
+
+    public static final Pose2d upperPositionOnTarmac =
+            new Pose2d(
+                    Units.inchesToMeters(250),
+                    Units.inchesToMeters(200),
+                    new Rotation2d(-38.65980825));
+    public static final Pose2d upperBall1 =
+            new Pose2d(
+                    Units.inchesToMeters(192.5),
+                    Units.inchesToMeters(242),
+                    new Rotation2d(-38.65980825));
+}
--- src/main/java/team3647/frc2022/autonomous/Sequences.java
+++ src/main/java/team3647/frc2022/autonomous/Sequences.java
@@ -1,3 +1,103 @@
 package team3647.frc2022.autonomous;
 
-public class Sequences {}
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.math.util.Units;
+import java.util.List;
+import team3647.frc2022.constants.DrivetrainConstants;
+
+// previous Trajectories
+public class Sequences {
+    private static final DifferentialDriveVoltageConstraint autoVoltageConstraint =
+            new DifferentialDriveVoltageConstraint(
+                    new SimpleMotorFeedforward(
+                            DrivetrainConstants.kS, DrivetrainConstants.kV, DrivetrainConstants.kA),
+                    DrivetrainConstants.kDriveKinematics,
+                    DrivetrainConstants.kNominalVoltage);
+
+    private static final TrajectoryConfig forwardTrajectoryConfig =
+            new TrajectoryConfig(
+                            DrivetrainConstants.kMaxSpeedMetersPerSecond,
+                            DrivetrainConstants.kMaxAccelerationMetersPerSecondSquared)
+                    .setKinematics(DrivetrainConstants.kDriveKinematics)
+                    .addConstraint(autoVoltageConstraint)
+                    .setReversed(false);
+    private static final TrajectoryConfig reverseTrajectoryConfig =
+            new TrajectoryConfig(
+                            DrivetrainConstants.kMaxSpeedMetersPerSecond,
+                            DrivetrainConstants.kMaxAccelerationMetersPerSecondSquared)
+                    .setKinematics(DrivetrainConstants.kDriveKinematics)
+                    .addConstraint(autoVoltageConstraint)
+                    .setReversed(true);
+    private static final TrajectoryConfig reverseTrajectoryConfigSlow =
+            new TrajectoryConfig(
+                            DrivetrainConstants.kMaxSpeedMetersPerSecond,
+                            AutoConstants.slowBoiAutoAccel)
+                    .setKinematics(DrivetrainConstants.kDriveKinematics)
+                    .addConstraint(autoVoltageConstraint)
+                    .setReversed(true);
+    private static final TrajectoryConfig forwardTrajectoryConfigFast =
+            new TrajectoryConfig(
+                            DrivetrainConstants.kMaxSpeedMetersPerSecond,
+                            AutoConstants.fastBoiAutoAccel)
+                    .setKinematics(DrivetrainConstants.kDriveKinematics)
+                    .addConstraint(autoVoltageConstraint)
+                    .setReversed(true);
+
+    // 5 ball
+    public static Trajectory tarmacToBottomLeftBall1 =
+            TrajectoryGenerator.generateTrajectory(
+                    AutoConstants.positionOnTarmacParallel,
+                    List.of(),
+                    AutoConstants.bottomLeftBall1,
+                    reverseTrajectoryConfigSlow);
+    public static Trajectory bottomLeftBall1ToTarmac =
+            TrajectoryGenerator.generateTrajectory(
+                    AutoConstants.bottomLeftBall1,
+                    List.of(),
+                    AutoConstants.positionOnTarmacParallel,
+                    forwardTrajectoryConfig);
+    public static Trajectory tarmacToBall2 =
+            TrajectoryGenerator.generateTrajectory(
+                    AutoConstants.positionOnTarmacParallel,
+                    List.of(),
+                    new Pose2d(
+                            AutoConstants.bottomLeftBall2,
+                            new Rotation2d(Units.degreesToRadians(-45))),
+                    reverseTrajectoryConfig);
+    public static Trajectory ball2ToLoad2 =
+            TrajectoryGenerator.generateTrajectory(
+                    new Pose2d(
+                            AutoConstants.bottomLeftBall2,
+                            new Rotation2d(Units.degreesToRadians(0))),
+                    List.of(),
+                    AutoConstants.bottomLeftBall3,
+                    reverseTrajectoryConfig);
+    public static Trajectory load2ToShoot =
+            TrajectoryGenerator.generateTrajectory(
+                    AutoConstants.bottomLeftBall3,
+                    List.of(),
+                    new Pose2d(
+                            AutoConstants.bottomLeftBall2,
+                            new Rotation2d(Units.degreesToRadians(45))),
+                    forwardTrajectoryConfigFast);
+
+    // 2 ball
+    public static Trajectory upperTarmacToUpperBall1 =
+            TrajectoryGenerator.generateTrajectory(
+                    AutoConstants.upperPositionOnTarmac,
+                    List.of(),
+                    AutoConstants.upperBall1,
+                    reverseTrajectoryConfig);
+    public static Trajectory upperBall1ToUpperTarmac =
+            TrajectoryGenerator.generateTrajectory(
+                    AutoConstants.upperBall1,
+                    List.of(),
+                    AutoConstants.upperPositionOnTarmac,
+                    forwardTrajectoryConfig);
+}
