dc6dee05d51f0d0c89f50573f905849aa62f89fb
Darmin T <im.darminator@gmail.com>
2022-02-25 16:06:03 -0800

    Update StatusLED.java

--- src/main/java/team3647/frc2022/subsystems/StatusLED.java
+++ src/main/java/team3647/frc2022/subsystems/StatusLED.java
@@ -4,11 +4,20 @@
 
 package team3647.frc2022.subsystems;
 
+import com.ctre.phoenix.led.CANdle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StatusLED extends SubsystemBase {
+    public static class Color {}
+
+    private final CANdle candle;
+
     /** Creates a new StatusLED. */
-    public StatusLED() {}
+    public StatusLED(CANdle candle) {
+        this.candle = candle;
+    }
+
+    public void setColor(Color color, double speed) {}
 
     @Override
     public void periodic() {
